4d59ff16319ab4d73d4e785666afc6bf69ce9e2c
William Pescherine <wkpescherine@gmail.com>
2019-05-29 03:42:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else
-      min_value = name_hash.first[1]
+      min_value = name_hash.first[0]
       min_key = name_hash.first[0]
       name_hash.each do |key, value|
         if value < min_value
